0e1d7e0e0b918c228363a85d2bbbd516a2092838
Logan <logankrieger317@gmail.com>
2024-02-14 08:25:30 -0600

    JS Examples and HTLM updated

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
+// First Program
+console.log('Welcome To JS Tutorial By Logan!');
+
+let year = 2024;
+
+if (year % 4 === 0) {
+  if (year % 100 !== 0) {
+    console.log("Leap year");
+  } else if (year % 400 === 0) {
+    console.log("Leap year");
+  } else {
+    console.log("Not a leap year");
+  }
+} else {
+  console.log("Not a leap year");
+}
+
+const button = document.getElementById("bg-button");
+button.addEventListener("click", function() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+
+});
 
 document.getElementById("demo").innerHTML = 5 + 6;

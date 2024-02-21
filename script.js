6801dc30e7aa8fd77df2933464d943c9c0d4d4b0
Joyce003 <148401034+Joyce003@users.noreply.github.com>
2024-02-21 00:21:25 -0600

    Update script.js
    added alert for clicking on puppy image and added payment information functions in JS

--- script.js
+++ script.js
@@ -1,77 +1,101 @@
-// First Program
-console.log('Welcome To JS Tutorial By Logan!');
+document.querySelectorAll('.image-gallery img').forEach(image => {
+    image.addEventListener('click', function() {
+        alert('You clicked on an image of an adorable puppy! Now you can have this fella for the low price of $5,000.');
+    });
+});
 
 
-let sum = 5 + 10;       // sum will be 15
-console.log(sum);
-let fullName = "John" + " Doe"; // fullName will be "John Doe"
-console.log(fullName);
-let difference = 20 - 8; // difference will be 12
-console.log(difference);
-let product = 6 * 7;     // product will be 42
-console.log(product);
-let quotient = 30 / 3;   // quotient will be 10
-console.log(quotient);
-let remainder = 17 % 5;  // remainder will be 2    
-console.log(remainder);
+<button type="button" onclick="showPaymentOptions()">Pay Now</button>
 
-console.log(4 == 4);      // Output: true
-console.log("dog" == "cat"); // Output: false
-
-console.log(4 != 4);
-console.log (4 === "four")      // Output: false
-
-let hasTicket = false;
-let isOver18 = true;
-
-if (hasTicket && isOver18) { 
-    console.log("You can enter the event!");
-} else {
-    console.log("Sorry, you can't enter the event!");
+<script>
+function showPaymentOptions() {
+    var paymentMethod = prompt("Enter 'PayPal' or 'Credit Card' to select your payment method.");
+    if (paymentMethod.toLowerCase() === "paypal") {
+        // Logic for PayPal payment
+       alert("You selected PayPal.");
+    } else if (paymentMethod.toLowerCase() === "credit card") {
+        // Logic for Credit Card payment
+        alert("You selected Credit Card.");
+    } else {
+        alert("Invalid selection. Please enter 'PayPal' or 'Credit Card'.");
+    }
 }
 
 
-// Conditional Statement
-
-let year = 2024;
-
-if (year % 4 === 0) {
-  if (year % 100 !== 0) {
-    console.log("Leap year");
-  } else if (year % 400 === 0) {
-    console.log("Leap year");
-  } else {
-    console.log("Not a leap year");
-  }
-} else {
-  console.log("Not a leap year");
-}
+// // First Program
+// console.log('Welcome To JS Tutorial By Logan!');
 
 
-let testScore = 90;
+// let sum = 5 + 10;       // sum will be 15
+// console.log(sum);
+// let fullName = "John" + " Doe"; // fullName will be "John Doe"
+// console.log(fullName);
+// let difference = 20 - 8; // difference will be 12
+// console.log(difference);
+// let product = 6 * 7;     // product will be 42
+// console.log(product);
+// let quotient = 30 / 3;   // quotient will be 10
+// console.log(quotient);
+// let remainder = 17 % 5;  // remainder will be 2    
+// console.log(remainder);
 
-if (testScore >= 90) {
-    console.log("A");
-} else if (testScore >= 80) {
-    console.log("B");
-} else if (testScore >= 70) {
-    console.log("C");
-} else if (testScore >= 60) {
-    console.log("D");
-} else {
-    console.log("F");
-}
+// console.log(4 == 4);      // Output: true
+// console.log("dog" == "cat"); // Output: false
 
-let cheatSheet = 3;
+// console.log(4 != 4);
+// console.log (4 === "four")      // Output: false
+
+// let hasTicket = false;
+// let isOver18 = true;
+
+// if (hasTicket && isOver18) { 
+//     console.log("You can enter the event!");
+// } else {
+//     console.log("Sorry, you can't enter the event!");
+// }
 
 
-if (cheatSheet >= 5) {
-    console.log("You have the cheat sheets!");
-}else if (cheatSheet >= 3) {
-    console.log("do you have more money?");
-}else {
-    console.log("You don't get the cheat sheets!");
-}
+// // Conditional Statement
+
+// let year = 2024;
+
+// if (year % 4 === 0) {
+//   if (year % 100 !== 0) {
+//     console.log("Leap year");
+//   } else if (year % 400 === 0) {
+//     console.log("Leap year");
+//   } else {
+//     console.log("Not a leap year");
+//   }
+// } else {
+//   console.log("Not a leap year");
+// }
+
+
+// let testScore = 90;
+
+// if (testScore >= 90) {
+//     console.log("A");
+// } else if (testScore >= 80) {
+//     console.log("B");
+// } else if (testScore >= 70) {
+//     console.log("C");
+// } else if (testScore >= 60) {
+//     console.log("D");
+// } else {
+//     console.log("F");
+// }
+
+// let cheatSheet = 3;
+
+
+// if (cheatSheet >= 5) {
+//     console.log("You have the cheat sheets!");
+// }else if (cheatSheet >= 3) {
+//     console.log("do you have more money?");
+// }else {
+//     console.log("You don't get the cheat sheets!");
+// }
 
 
 
